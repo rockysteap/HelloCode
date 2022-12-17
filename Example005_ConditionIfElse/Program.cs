51758eb4bfe7985208bbcbb8e96d62df4f66ad5e
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-17 23:59:08 +0300

    fix(example_005): add null-forgiving operator

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,6 +1,6 @@
 ﻿// ConditionIfElse
 Console.WriteLine("Введите имя пользователя: ");
-string username = Console.ReadLine();
+string username = Console.ReadLine()!;
 if (username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША");
